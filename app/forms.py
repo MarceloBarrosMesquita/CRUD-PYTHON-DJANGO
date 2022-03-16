c26629b40b63279072b30c8ffdd74a70d5814d94
Marcelo Mesquita <marcelo.mesquita@voxline.com.br>
2022-03-16 16:49:33 -0300

    First commit

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from app.models import Alunos
+
+class AlunosForm(ModelForm):
+    class Meta:
+        model = Alunos
+        fields = ['nome', 'curso', 'idade']
